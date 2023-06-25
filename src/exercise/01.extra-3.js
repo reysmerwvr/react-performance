6b2e9dfdd09daecb6fa93cde64feda73c4b0c3e9
Reysmer Valle <reysmerwvr@gmail.com>
2023-06-25 17:33:50 +0200

    complete code splitting exercise

--- src/exercise/01.extra-3.js
+++ src/exercise/01.extra-3.js
@@ -3,7 +3,7 @@
 
 import * as React from 'react'
 
-const Globe = React.lazy(() => import('../globe'))
+const Globe = React.lazy(() => import(/* webpackPrefetch: true */ '../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
@@ -27,11 +27,11 @@ function App() {
         />
         {' show globe'}
       </label>
-      <div style={{width: 400, height: 400}}>
-        <React.Suspense fallback={<div>loading...</div>}>
+      <React.Suspense fallback={<div>loading...</div>}>
+        <div style={{width: 400, height: 400}}>
           {showGlobe ? <Globe /> : null}
-        </React.Suspense>
-      </div>
+        </div>
+      </React.Suspense>
     </div>
   )
 }
